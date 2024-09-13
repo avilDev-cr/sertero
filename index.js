847659c6b9abab9a2bb9d2fa13f4a19d4283305d
Andres Aviles <andrezavilesrdz@gmail.com>
2024-09-13 07:33:36 -0600

    corrigiendo funcionalidad del menu

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.getElementById('hamburger');
+    const hamburgerMenu = document.getElementById('hamburgerMenu');
+    const menuItems = hamburgerMenu.querySelectorAll('a');
 
-// Abrir/Cerrar menú hamburguesa
-const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('hamburgerMenu');
+    // Alterna la clase 'open' en el menú cuando se hace clic en el ícono
+    hamburger.addEventListener('click', function() {
+        hamburgerMenu.classList.toggle('open');
+    });
 
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('active');
+    // Cierra el menú cuando se selecciona una opción
+    menuItems.forEach(function(item) {
+        item.addEventListener('click', function() {
+            hamburgerMenu.classList.remove('open');
+        });
+    });
 });
